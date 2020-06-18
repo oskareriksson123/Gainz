69cb4417ba3a2a81b3d85c63fb287ddad8617108
oskareriksson123 <47111141+oskareriksson123@users.noreply.github.com>
2020-06-18 18:12:41 +0200

    work 2

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const IndexRouter = require('./controllers/index');
 const mongoose = require('mongoose');
-
+const RegisterRouter = require('./controllers/register');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
-app.set('layout','includes.ejs');
+app.set('layout','baseLayout.ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use('/', IndexRouter);
+app.use('/register', RegisterRouter);
 
 mongoose.connect(process.env.DATABASE_URL,  {useNewUrlParser: true,useUnifiedTopology: true});
 const db = mongoose.connection;
